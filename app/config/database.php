433e0b41beac9a98cc95ae62b0705c164d60fd5a
Yuom Theara <yuom.theara@gmail.com>
2015-02-12 13:50:32 +0700

    update AdminLTE

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'cpanel2',
+			'database'  => 'cpanel',
 			'username'  => 'root',
 			'password'  => '123456',
 			'charset'   => 'utf8',
